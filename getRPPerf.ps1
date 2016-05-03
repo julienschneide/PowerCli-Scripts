8a2b987881b404ea15d147e525fab667709d197f
julienschneide <julien.schneider@he-arc.ch>
2016-05-03 14:25:51 +0200

    Sort

--- getRPPerf.ps1
+++ getRPPerf.ps1
@@ -22,14 +22,18 @@ $RPs = get-ResourcePool -Name $selectedRP -Location $selectedHost
 
 foreach($RP in $RPs)
     {
-     Write-Host "Collecting data for" $RP " resource pool..."
-	 $statistics += Get-Stat -Entity $RP -Stat $metrics_rp -Start $sDate -Finish $fDate -IntervalMins $interval
-	 #$statistics += RP.CpuLimitMhz
-	 Write-Host "CPU Limit Mhz :" $RP.CpuLimitMhz
-	 Write-Host "CPU Reservation Mhz :" $RP.CpuReservationMhz
-	 Write-Host "Memory Limit MB" $RP.MemLimitMB
-	 Write-Host "Memory Reservation Limit MB" $RP.MemReservationMB
+	#$RPstat = "" | SELECT CpuLimitMhz, MemLimitMB
+	#$RPstat.CpuLimitMhz = $RP.CpuLimitMhz
+	 
+	 
+	Write-Host "Collecting data for" $RP " resource pool on" $selectedHost "Host..."
+	$statistics += Get-Stat -Entity $RP -Stat $metrics_rp -Start $sDate -Finish $fDate -IntervalMins $interval
+	#$statistics += $RPstat
+	Write-Host "CPU Limit Mhz :" $RP.CpuLimitMhz
+	Write-Host "CPU Reservation Mhz :" $RP.CpuReservationMhz
+	Write-Host "Memory Limit MB" $RP.MemLimitMB
+	Write-Host "Memory Reservation Limit MB" $RP.MemReservationMB
     }
 	
 #$statistics | SELECT Timestamp, Entity, MetricId, Unit, Value | Export-CSV -Path .\exports\RPPerf.csv -Force -NoTypeInformation
-$statistics | Export-CSV -Path .\exports\RPPerf.csv -Force -NoTypeInformation
+$statistics | Sort-Object Entity, MetricId, Timestamp | Export-CSV -Path .\exports\RPPerf.csv -Force -NoTypeInformation
